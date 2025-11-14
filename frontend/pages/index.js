6aedc374b7ed0b2b76c6118a0d6651a763f6f6bb
Santiago García <santiagogar2053@gmail.com>
2025-11-14 16:11:19 -0300

    docs(frontend): point to backend on port 8081

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div style={{fontFamily: 'sans-serif', padding: 20}}>
       <h1>Hola Mundo desde el frontend!</h1>
-      <p>Try connecting to backend at <code>http://localhost:8080/</code></p>
+      <p>Try connecting to backend at <code>http://localhost:8081/</code></p>
     </div>
   )
 }
